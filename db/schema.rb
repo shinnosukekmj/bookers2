fa5f0dbe281fab9c422a46345da399f797abe66b
EC2 Default User <ec2-user@ip-172-31-39-48.ec2.internal>
2022-01-19 04:18:10 +0000

    finish2

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_16_053753) do
+ActiveRecord::Schema.define(version: 2022_01_19_040301) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -44,6 +44,12 @@ ActiveRecord::Schema.define(version: 2022_01_16_053753) do
     t.string "title"
     t.text "body"
     t.integer "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "names", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
